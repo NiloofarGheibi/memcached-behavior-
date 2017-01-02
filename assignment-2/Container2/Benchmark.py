103c40a0f4c60889ff8992920e6e86d88df4d540
NiloofarGheibi <niloofar.gheibi@gmail.com>
2017-01-02 19:22:21 +0100

    Resolving issues for finding and connecting to Server Container. Resolving issues for mcperf commands

--- assignment-2/Container2/Benchmark.py
+++ assignment-2/Container2/Benchmark.py
@@ -6,10 +6,7 @@ def prepare_exp(SSHHost, SSHPort, REMOTEROOT, optpt):
     f.write("Host benchmark\n")
     f.write("   Hostname %s\n" % SSHHost)
     f.write("   Port %d\n" % SSHPort)
-    f.close()
-    
-
-    f.write("   IdentityFile /root/.ssh/id_rsa\n")
+    f.write("   IdentityFile id_rsa\n")
     f.write("   StrictHostKeyChecking no\n")
     f.close()
 
@@ -19,9 +16,13 @@ def prepare_exp(SSHHost, SSHPort, REMOTEROOT, optpt):
     
    #f.write("ssh -F config benchmark \"nohup ls dummy -p 11222 -P memcached.pid > memcached.out 2> memcached.err &\"\n") # adjust this line to properly start memcached
     
+    f.write("cp -i /root/.ssh/id_rsa id_rsa\n")
+
     f.write("ssh -F config benchmark \"memcached -d -u memcache -P /mm/memcached.pid > /mm/memcached.out 2> /mm/memcached.err &\"\n")
 
-    f.write("RESULT=`ssh -F config benchmark \"pidof memcachd\"`\n")
+    #f.write("RESULT=`ssh -F config benchmark \"pidof memcachd\"`\n")
+
+    f.write("RESULT=`ssh -p 10022 server \"pidof memcached\"`\n")
 
     f.write("sleep 5\n")
 
@@ -31,7 +32,9 @@ def prepare_exp(SSHHost, SSHPort, REMOTEROOT, optpt):
     
     # add a few lines to extract the "Response rate" and "Response time \[ms\]: av and store them in $REQPERSEC and $LATENCY"
 	
-    f.write("mcperf --linger=0 --timeout=5 --conn-rate=%d --call-rate=1000 --num-calls=6 --num-conns=50 --sizes=u1,16 2>outputest_%d.txt\n" % (optpt["rate"], optpt["rate"]))
+    #f.write("mcperf --timeout=5 --conn-rate=%d --call-rate=1000 --num-calls=6 --num-conns=50 --sizes=u1,16 -s %s 2>outputest_%d.txt\n" % (optpt["rate"], SSHHost, optpt["rate"]))
+
+    f.write("mcperf --timeout=5 --conn-rate=%d --call-rate=1000 --num-calls=6 --num-conns=50 --sizes=u1,16 -s %s 2>outputest_%d.txt\n" % (optpt["rate"], SSHHost, optpt["rate"]))
 
     f.write("REQPERSEC=$(cat outputest_%d.txt | grep \"Response rate:\" | cut -c16- | sed 's/ .*//')\n" % (optpt["rate"]) )
 
@@ -41,13 +44,13 @@ def prepare_exp(SSHHost, SSHPort, REMOTEROOT, optpt):
 
     f.write("MIN_LATENCY=$(cat outputest_%d.txt | grep \"Response time \[ms]: p25\" | cut -c25- | sed 's/ .*//')\n" % (optpt["rate"]))
 
-    f.write("ssh -F config benchmark \"sudo kill -9 $(cat /mm/memcached.pid)\"\n")
+    f.write("ssh -F config benchmark \"kill -9 $(cat /mm/memcached.pid)\"\n")
 
     f.write("echo \"requests,latency,max_latency,min_latency\" > stats.csv\n")
     
     f.write("echo \"$REQPERSEC,$LATENCY,$MAX_LATENCY,$MIN_LATENCY\" >> stats.csv\n")
     
-    f.write("scp -F config benchmark:~/mm/memcached.* .\n")
+    f.write("scp -F config benchmark:/mm/memcached.* .\n")
 
     f.write("if [[ $(wc -l <stats.csv) -le 1 ]]; then CODE=1; fi\n\n")
     
